8bd7dfe39d14090fb70531ffe5edfdfb14602f6e
Tricia Amheiser <tricia@taarts.net>
2022-10-07 13:39:53 -0400

    resolved TS errors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,18 @@ import './style.css'
 //
 // because the event tells us the TARGET
 // target is the thing we clicked on
-function handleClickSquare(event) {
+function handleClickSquare(event: MouseEvent) {
   const thingCLickedOn = event.target
 
-  thingCLickedOn.textContent = 'X'
+  // If the thing clicked on is an LI Element
+  // - This does several things:
+  // - 1. Checks that the target isn't null
+  // - 2. Let's TypeScript know that *inside* this if statement
+  //      the thingClickedOn is an LI element, and thus we can
+  //      change its textContent
+  if (thingCLickedOn instanceof HTMLLIElement) {
+    thingCLickedOn.textContent = 'X'
+  }
 }
 // firstListItem?.addEventListener('click', handleClickSquare)
 
