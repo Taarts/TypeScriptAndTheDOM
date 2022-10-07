77720cb47b762b038107e1f746a22fedde60206d
Tricia Amheiser <tricia@taarts.net>
2022-10-07 13:27:01 -0400

    handles all the "li" in the page

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import './style.css'
+
+// const firstListItem = document.querySelector('li')
+
+// generic and works for ANY 'li'
+//
+// because the event tells us the TARGET
+// target is the thing we clicked on
+function handleClickSquare(event) {
+  const thingCLickedOn = event.target
+
+  thingCLickedOn.textContent = 'X'
+}
+// firstListItem?.addEventListener('click', handleClickSquare)
+
+const allSquares = document.querySelectorAll('li')
+
+allSquares.forEach((square) =>
+  square.addEventListener('click', handleClickSquare)
+)
+// console.log(allSquares)
